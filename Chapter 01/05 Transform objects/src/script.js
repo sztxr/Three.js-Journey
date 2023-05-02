1eb35c4ee96713a7c90d4fdadce123fb13b66b4d
sztxr <sztxr@github.com>
2023-05-01 22:01:04 -0300

    lesson 05 - scene graph

--- Chapter 01/05 Transform objects/src/script.js
+++ Chapter 01/05 Transform objects/src/script.js
@@ -7,42 +7,33 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Objects
+ * Objects and Groups
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
 
-/**
- * Position
- */
-// mesh.position.x = 0.7; // to the right
-// mesh.position.y = -0.6; // down
-// mesh.position.y = 1; // closer to the camera
-mesh.position.set(0.7, -0.6, 1); // set x, y and z at once
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+);
+group.add(cube1);
 
-// distance from the mesh to center of the scene
-console.log(mesh.position.length());
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+);
+cube2.position.x = -2;
+group.add(cube2);
 
-// reduces the length of the vector to 1 unit but preserving its direction
-mesh.position.normalize();
-console.log(mesh.position.length());
-
-/**
- * Scale
- */
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.25;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
-
-/**
- * Rotation
- */
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 /**
  * Axes Helper
@@ -66,12 +57,6 @@ camera.position.z = 3;
 // camera.position.y = 1;
 // camera.position.x = 1;
 scene.add(camera);
-
-// distance from the camera and the object
-console.log(mesh.position.distanceTo(camera.position));
-
-// look at the object
-camera.lookAt(mesh.position);
 
 /**
  * Renderer
